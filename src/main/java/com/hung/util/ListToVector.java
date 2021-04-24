7dc8f3d819646365c5fefaab0c8535907d07b639
Hung <513252767@qq.com>
2021-04-24 10:30:24 +0800

    fix(view):修改课程表显示bug

--- src/main/java/com/hung/util/ListToVector.java
+++ src/main/java/com/hung/util/ListToVector.java
@@ -29,25 +29,27 @@ public class ListToVector {
         final int NUM = 6;
         UserService userService = new ServiceFactory<>(new UserServiceImpl()).getService();
         Vector<Vector> objects = new Vector<>();
-        for (int i = 0; i < list.size(); i++) {
-            Lesson lesson = list.get(i);
 
             for (int j = 1; j < NUM; j++) {
                 Map<String, String> map = new LinkedHashMap();
                 map.put("第一节课", "第" + j + "节课");
                 for (int k = 1; k < NUM; k++) {
-                    if (Integer.parseInt(lesson.getTurn()) == j && Integer.parseInt(lesson.getWeek()) == k) {
-                        String teacher = userService.queryNickNameById(Integer.valueOf(lesson.getTeacher()));
-                        map.put(j + k + "", lesson.getName() + "@" + lesson.getClassroom() + teacher + "");
-                    } else {
-                        map.put(j + k + "", "");
+                    for (int i = 0; i < list.size(); i++) {
+                        Lesson lesson = list.get(i);
+                        if (Integer.parseInt(lesson.getTurn()) == j && Integer.parseInt(lesson.getWeek()) == k) {
+                            String teacher = userService.queryNickNameById(Integer.valueOf(lesson.getTeacher()));
+                            map.put(j + k + "", lesson.getName() + "@" + lesson.getClassroom() + teacher + "");
+                            break;
+                        } else {
+                            map.put(j + k + "", "");
+                        }
                     }
                 }
                 Vector vector = new Vector();
                 vector.addAll(map.values());
                 objects.add(vector);
             }
-        }
+
         return objects;
     }
 
